d8bfb5a141af311df7bb074644705951c58cbfcc
Margo <solitude6060@gmail.com>
2019-07-08 01:30:47 +0800

    Add view, url and template tag

--- ocamp/myapp/views.py
+++ ocamp/myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from datetime import datetime
 
 # Create your views here.
+
+def hello_world(request):
+    return HttpResponse("Hello World!")
+
+def index(request):
+    name = "Pika~"
+    current_time = str(datetime.now())
+    return render(request, 'myapp/index.html', locals())
+
+def tag_page(request):
+    animals = ['cat', 'dog', 'pikachu'] 
+    return render(request, 'myapp/tag.html', locals())
